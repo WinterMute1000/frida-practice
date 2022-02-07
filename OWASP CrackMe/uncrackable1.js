655fce189902b6605f7cb9f1357bfe36ac5b32a6
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2022-02-07 15:31:16 +0900

    Solve Uncrackable 1

---
+++ OWASP CrackMe/uncrackable1.js
@@ -0,0 +1,40 @@
+'use strict'
+
+function byPassRoot(){
+    var rootDetecter = Java.use('sg.vantagepoint.a.c');
+    rootDetecter.a.overload().implementation=()=>{
+            return false;
+    }
+    rootDetecter.b.overload().implementation=()=>{
+            return false;
+    }
+    rootDetecter.c.overload().implementation=()=>{
+        return false;
+    }
+}
+
+function decryptCode(){
+    var androidBase64 = Java.use('android.util.Base64');
+    var sgVantagepointAA = Java.use('sg.vantagepoint.a.a');
+    var sgVantagepointUncrackable1A = Java.use('sg.vantagepoint.uncrackable1.a');
+
+    var arrayOfByte = androidBase64.decode("5UJiFctbmgbDoLXmpL12mkno8HT4Lv8dlat8FxR2GOc=", 0);
+    arrayOfByte = sgVantagepointAA.a(sgVantagepointUncrackable1A.b("8d127684cbc37c17616d806cf50473cc"), arrayOfByte);
+
+    console.log(arrayOfByte.toString());
+}
+if(Java.available){
+    Java.perform(function(){
+        try{
+            byPassRoot();
+            decryptCode();
+        }
+        catch(error){
+            console.log("[-] An exception occured");
+            console.log(String(error.stack));
+        }
+    });
+}
+else{
+    console.log("[-] Java is not available");
+}
